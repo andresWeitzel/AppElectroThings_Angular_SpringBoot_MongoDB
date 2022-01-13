0ef1c9813cc9543f1529964685b7fa79436e4ac5
andres <andres06energy@hotmail.com>
2022-01-13 17:45:18 -0300

    Updated Carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarModule } from './components/navbar/navbar.module';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { CarouselModule } from './components/carousel/carousel.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NavbarModule
+    NavbarModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
