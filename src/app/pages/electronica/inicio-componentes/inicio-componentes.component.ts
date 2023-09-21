8eff4e61503ba586a9d3e9356d1d4a88bf931dcd
Andrés Weitzel <andres96energy@hotmail.com>
2023-09-21 20:15:14 -0300

    Changed img and src

--- src/app/pages/electronica/inicio-componentes/inicio-componentes.component.ts
+++ src/app/pages/electronica/inicio-componentes/inicio-componentes.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio-componentes.component.scss']
 })
 export class InicioComponentesComponent implements OnInit {
-//Migrate here
+  //ImgPaths
+  imgHelp = "/assets/images/help02.png";
+  imgBgRedBlack01 = "/assets/background/red-back-10.jpg";
+  imgBgRedBlack02 = "/assets/background/red-back-01.jpg";
+  imgBgRedBlue = "/assets/background/red-back-12.jpg";
+  imgBooks = "assets/images/list-products.png";
+  imgPackage = "assets/images/package.png";
+  imgHand = "assets/images/edit.png";
+  imgGraphics = "assets/images/graphics02.png";
+  imgNotebook = "assets/images/productsAbout.png";
+  imgMegaphone = "assets/images/megaphone.png";
+
+
+
+
   constructor() { }
 
   ngOnInit(): void {
